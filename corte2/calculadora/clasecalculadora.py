35d5d1324635af1cea735442dfc232c2a042a7bd
JuanBallesteros663 <juand.ballesr@gmail.com>
2025-05-11 21:24:16 -0500

    Create clasecalculadora.py

---
+++ corte2/calculadora/clasecalculadora.py
@@ -0,0 +1,19 @@
+# calculadora.py
+
+class Calculadora:
+    def __init__(self):  # corregido: __init__ en lugar de _init_
+        pass
+
+    def sumar(self, a, b):
+        return a + b
+
+    def restar(self, a, b):
+        return a - b
+
+    def multiplicar(self, a, b):
+        return a * b
+
+    def dividir(self, a, b):
+        if b == 0:
+            raise ZeroDivisionError("No se puede dividir entre cero.")
+        return a / b
